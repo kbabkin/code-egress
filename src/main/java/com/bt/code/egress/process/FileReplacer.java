9143301932b83e56f7c9ccbbf087c546e909dc76
Konstantin Babkin <kostya_babkin@mail.ru>
2022-08-22 11:13:04 +0300

    Ignore binary files

--- src/main/java/com/bt/code/egress/process/FileReplacer.java
+++ src/main/java/com/bt/code/egress/process/FileReplacer.java
@@ -1,12 +1,14 @@
 package com.bt.code.egress.process;
 
 import com.bt.code.egress.read.LineLocation;
+import com.bt.code.egress.read.LineToken;
 import com.bt.code.egress.write.FileCompleted;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.MalformedInputException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,10 +24,16 @@ public class FileReplacer {
         int lineNum = 0;
         ArrayList<String> originalLines = new ArrayList<>();
         List<String> replacedLines = new ArrayList<>();
-        while (((line = bufferedReader.readLine()) != null)) {
-            String replace = lineReplacer.replace(line, new LineLocation(file, ++lineNum), textMatchedListener);
-            originalLines.add(line);
-            replacedLines.add(replace);
+        try {
+            while (((line = bufferedReader.readLine()) != null)) {
+                String replace = lineReplacer.replace(line, new LineLocation(file, ++lineNum), textMatchedListener);
+                originalLines.add(line);
+                replacedLines.add(replace);
+            }
+        } catch (MalformedInputException e) {
+            log.error("Failed to read file {}", file, e);
+            textMatchedListener.onMatched(new TextMatched(new LineLocation(file, 0),
+                    new LineToken(""), null, "", "FAILED to read file " + file));
         }
         return new FileCompleted(file, originalLines, replacedLines);
     }
